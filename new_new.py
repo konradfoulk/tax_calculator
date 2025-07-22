89eabbfa5f8112ba8c5341f44a2d162a91ee783c
konradfoulk <konrad.foulk@gmail.com>
2025-07-22 12:20:14 -0400

    binary search to find required income

--- new_new.py
+++ new_new.py
@@ -1,5 +1,3 @@
-self_employed = False
-
 brackets = [
     (0, 11600, 0.10),
     (11600, 47150, 0.12),
@@ -10,17 +8,11 @@ brackets = [
     (609350, float('inf'), 0.37)
 ]
 
-if self_employed:
-    ss_rate = 0.124
-    medicare_rate = 0.029
-else:
-    ss_rate = 0.062
-    medicare_rate = 0.0145
 
-standard_deduction = 14600
+def get_income_tax(gross_income):
+    standard_deduction = 14600
 
-
-def get_income_tax(taxable_income):
+    taxable_income = gross_income - standard_deduction
     tax = 0
     for lower, upper, rate in brackets:
         if taxable_income > lower:
@@ -32,7 +24,14 @@ def get_income_tax(taxable_income):
     return tax
 
 
-def get_fica_tax(income):
+def get_fica_tax(income, self_employed):
+    if self_employed:
+        ss_rate = 0.124
+        medicare_rate = 0.029
+    else:
+        ss_rate = 0.062
+        medicare_rate = 0.0145
+
     ss_tax = min(income, 168600) * ss_rate
     medicare_tax = income * medicare_rate
     if income > 200000:
@@ -42,14 +41,27 @@ def get_fica_tax(income):
     return ss_tax + medicare_tax
 
 
-gross_income = 250000
-taxable_income = gross_income - standard_deduction
+def get_after_tax_income(income):
+    income_tax = get_income_tax(income)
+    fica_tax = get_fica_tax(income, False)
 
-income_tax = get_income_tax(taxable_income)
-fica_tax = get_fica_tax(gross_income)
-after_tax_income = gross_income - income_tax - fica_tax
+    return income - income_tax - fica_tax
 
-print(gross_income)
-print(income_tax)
-print(fica_tax)
+
+def find_after_tax_income(desired_income, tolerance=1):
+    low = desired_income
+    high = desired_income / (1 - brackets[-1][-1])
+    while (high - low) > tolerance:
+        mid = (low + high) / 2
+        mid_after_tax = get_after_tax_income(mid)
+        if mid_after_tax < desired_income:
+            low = mid
+        else:
+            high = mid
+    return high
+
+
+required_income = find_after_tax_income(600000)
+print(required_income)
+after_tax_income = get_after_tax_income(required_income)
 print(after_tax_income)
